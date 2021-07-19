8268582346d43de7b2e4c9ad04811757dccfac1e
Smartfeld.ch <78407877+smartfeld-ch@users.noreply.github.com>
2021-07-19 12:05:48 +0200

    Min und Max Temperatur anzeigen

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-//empty
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+    basic.clearScreen()
+})
+let min = 0
+let max = 0
+let currenttemperature = input.temperature()
+max = currenttemperature
+min = currenttemperature
+basic.forever(function () {
+    basic.showString(".")
+    currenttemperature = input.temperature()
+    if (currenttemperature < min) {
+        min = currenttemperature
+    }
+    if (currenttemperature > max) {
+        max = currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+})
